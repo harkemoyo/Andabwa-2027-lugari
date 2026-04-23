e873bd18f3920fcc17c4b95162f2f914b15f5165
Omu34 <bomuse693@gmail.com>
2026-04-23 15:32:37 +0300

    social links update

--- storage/framework/views/499c2979693afea3abd2652d40a0f296.php
+++ storage/framework/views/499c2979693afea3abd2652d40a0f296.php
@@ -1,18 +1,24 @@
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8 w-full">
-    <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php \Livewire\Features\SupportCompiledWireKeys\SupportCompiledWireKeys::openLoop(); ?><?php endif; ?><?php $__currentLoopData = $radioChannels; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $radio): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?><?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><?php \Livewire\Features\SupportCompiledWireKeys\SupportCompiledWireKeys::startLoopIteration(); ?><?php endif; ?>
-    <div class="bg-white rounded-2xl shadow-sm border  border-gray-100 overflow-hidden group p-4 ">
+
+
+<div>
+    
+
+
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 p-8 w-full">
+    <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php \Livewire\Features\SupportCompiledWireKeys\SupportCompiledWireKeys::openLoop(); ?><?php endif; ?><?php $__currentLoopData = $tvChannels; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $tv): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?><?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><?php \Livewire\Features\SupportCompiledWireKeys\SupportCompiledWireKeys::startLoopIteration(); ?><?php endif; ?>
+    <div class="bg-white rounded-2xl shadow-sm border  border-gray-100 overflow-hidden group">
         <div class="relative h-48">
 
             <div class="absolute inset-0 z-0">
-                <img src="<?php echo e($radio->full_cover_image_path); ?>"
-                    alt="<?php echo e($radio->title); ?>"
-                    class="h-full w-full object-cover rounded-lg">
+                <img src="<?php echo e($tv->full_cover_image_path); ?>"
+                    alt="<?php echo e($tv->title); ?>"
+                    class="h-full w-full object-cover">
                 <div class="absolute inset-0 bg-gradient-to-b from-black/40 via-black/20 to-black/60"></div>
             </div>
             <div class="ustify-center items-center">
-                <img src="<?php echo e($radio->full_cover_image_path); ?>" alt="" class="w-20 h-20 rounded-full object-contain object-center pointer-events-none">
+                <img src="<?php echo e($tv->full_cover_image_path); ?>" alt="" class="w-20 h-20 rounded-full object-contain object-center pointer-events-none">
             </div>
-            <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php if($radio->type === 'live'): ?>
+            <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php if($tv->type === 'live'): ?>
             <span class="absolute top-4 right-4 bg-red-600 text-white text-[10px] font-bold px-2 py-1 rounded-full animate-pulse">
                 LIVE
             </span>
@@ -20,19 +26,19 @@
         </div>
 
         <div class="p-6">
-            <h3 class="text-xl font-bold text-gray-900 group-hover:text-indigo-600 transition"><?php echo e($radio->title); ?></h3>
-            <p class="text-gray-500 text-sm mt-2 line-clamp-2"><?php echo e($radio->description); ?></p>
+            <h3 class="text-xl font-bold text-gray-900 group-hover:text-indigo-600 transition"><?php echo e($tv->title); ?></h3>
+            <p class="text-gray-500 text-sm mt-2 line-clamp-2"><?php echo e($tv->description); ?></p>
 
             <div class="mt-6 flex items-center justify-between">
-                <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php if($radio->type === 'upload'): ?>
+                <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if BLOCK]><![endif]--><?php endif; ?><?php if($tv->type === 'upload'): ?>
                 <button class="flex items-center gap-2 text-indigo-600 font-semibold text-sm">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" />
                     </svg>
-                    Listen Now (<?php echo e($radio->duration_minutes); ?>m)
+                    Listen Now (<?php echo e($tv->duration_minutes); ?>m)
                 </button>
                 <?php else: ?>
-                 <a href="<?php echo e($radio->live_url); ?>" target="_blank" rel="noopener noreferrer"
+                 <a href="<?php echo e($tv->live_url); ?>" target="_blank" rel="noopener noreferrer"
                         class="inline-flex items-center justify-center w-full bg-gray-900 text-white px-4 py-3 rounded-xl text-sm font-bold hover:bg-indigo-600 transition-colors shadow-sm active:scale-[0.98]">
                         Join Live Stream
                         <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
@@ -46,7 +52,8 @@
     <?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><?php \Livewire\Features\SupportCompiledWireKeys\SupportCompiledWireKeys::endLoop(); ?><?php endif; ?><?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><?php if(\Livewire\Mechanisms\ExtendBlade\ExtendBlade::isRenderingLivewireComponent()): ?><!--[if ENDBLOCK]><![endif]--><?php \Livewire\Features\SupportCompiledWireKeys\SupportCompiledWireKeys::closeLoop(); ?><?php endif; ?>
 
     <div class="col-span-full">
-        <?php echo e($radioChannels->links()); ?>
+        <?php echo e($tvChannels->links()); ?>
 
     </div>
-</div><?php /**PATH C:\Users\Rygss\Downloads\andabwa-2027\resources\views/livewire/radio-list.blade.php ENDPATH**/ ?>
+</div>
+</div><?php /**PATH C:\Users\Rygss\Downloads\andabwa-2027\resources\views/livewire/tv-list.blade.php ENDPATH**/ ?>
