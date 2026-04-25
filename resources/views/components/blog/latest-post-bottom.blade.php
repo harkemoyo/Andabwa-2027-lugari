cfeb8c8206fe0e68445b7d3cb8849e81ddb73765
Omu34 <bomuse693@gmail.com>
2026-04-25 17:41:29 +0300

    chore: update composer.lock to fix deployment mismatch

--- resources/views/components/blog/latest-post-bottom.blade.php
+++ resources/views/components/blog/latest-post-bottom.blade.php
@@ -20,7 +20,8 @@
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
         </svg>
-    </button>
+    </\
+    button>
 
     {{-- Right Arrow --}}
     <button 
